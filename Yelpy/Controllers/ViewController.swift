b114523ded38037d364ebcfd91c5fd829e4da311
Stephen Tan <sttan@ucsd.edu>
2021-02-20 19:13:33 -0800

    Added tableview

--- Yelpy/Controllers/ViewController.swift
+++ Yelpy/Controllers/ViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var tableView: UITableView!
+    var restaurantsArray = [Restaurant]()
 
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
         
+        tableView = UITableView()
+        tableView.rowHeight = 150
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
         
         self.view = view
     }
@@ -22,8 +35,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         title = "Yelpy"
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(RestaurantCell.self, forCellReuseIdentifier: "restaurant")
+        
+        restaurantsArray.append(Restaurant.init(name: "name", address: "wow"))
+        tableView.reloadData()
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "restaurant", for: indexPath) as! RestaurantCell
+        let restaurant = restaurantsArray[indexPath.row]
+        
+        cell.restaurant = restaurant
+        return cell
+    }
+    
+}
